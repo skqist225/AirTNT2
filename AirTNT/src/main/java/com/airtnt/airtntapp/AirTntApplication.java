ae3f66d164ae5bc159b99d1644c121fac8834d95
skqist225 <74346301+skqist225@users.noreply.github.com>
2021-12-11 10:41:29 +0700

    add import

--- AirTNT/src/main/java/com/airtnt/airtntapp/AirTntApplication.java
+++ AirTNT/src/main/java/com/airtnt/airtntapp/AirTntApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ComponentScan({ "com.airtnt.airtntapp" })
