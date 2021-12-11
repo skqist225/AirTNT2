c8c29d9bff19fe9646013b8292faa797d4e9e0a6
skqist225 <74346301+skqist225@users.noreply.github.com>
2021-12-11 12:00:07 +0700

    static url

--- AirTNT/mysql_tomcat.sh
+++ AirTNT/mysql_tomcat.sh
@@ -96,10 +96,9 @@ systemctl enable tomcat
 cd /tmp/AirTNT2/AirTNT/
 mvn install
 systemctl stop tomcat
-sleep 30
 rm -rf /opt/tomcat/latest/webapps/ROOT*
 cp /tmp/AirTNT2/AirTNT/target/AirTNT-0.0.1-SNAPSHOT.war /opt/tomcat/latest/webapps/ROOT.war
 systemctl start tomcat
-sleep 30
+sleep 10
 cp /tmp/AirTNT2/AirTNT/src/main/resources/application.properties /opt/tomcat/latest/webapps/ROOT/WEB-INF/classes/application.properties
 systemctl restart tomcat
